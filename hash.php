cabc85b1ac87577ffb991c22266143ffc6937c4c
yosnak13 <yosnak20@gmail.com>
2020-11-05 23:49:18 +0900

    連想配列

---
+++ hash.php
@@ -0,0 +1,17 @@
+<?php
+// $data = [
+//   "name"=>"大泉",
+//   "gender"=>"男性",
+//   "age"=>46
+// ];
+// $data['children'] = ["太郎", "次郎", "三郎"];
+
+// var_dump($data)
+
+$dic = [
+  "x"=> 10,
+  "y"=> 20
+];
+$dic["y"] = 40;
+var_dump($dic)
+?>
