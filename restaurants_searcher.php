dbc202d5d5fc9184a2fd5a4d136172d2cb2fb64c
yosnak13 <yosnak20@gmail.com>
2020-11-08 16:34:21 +0900

    restaurant_sercherにてAPI組み込み

--- restaurants_searcher.php
+++ restaurants_searcher.php
@@ -2,22 +2,44 @@
 require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
+# 初期設定
+$KEYID = "1f19fb2ec4009251fef8fa7b4353473f";
+$HIT_PER_PAGE = 100;
+$PREF = "PREF13";
+$FREEWORD_CONDITION = 1;
+$FREEWORD = "渋谷駅";
 
-function write_data_to_csv(){
-  $restaurants = [];
-  $response = "hugahuga";
+$PARAMS = array("keyid"=> $KEYID, "hit_per_page"=>$HIT_PER_PAGE, "pref"=>$PREF, "freeword_condition"=>$FREEWORD_CONDITION, "freeword"=>$FREEWORD);
 
-  if(isset($response["error"])){
-    return print("エラーが発生しました!");
-  }
-  if(isset($response["rest"])){
-    foreach($response["rest"] as &$i){
-      $restaurants_name = $i["name"];
-      $restaurants[] = $restaurants_name;
+
+function write_data_to_csv($params){
+
+    $restaurants = [["名称","住所","営業日","電話番号"]];
+    $client = new Client();
+    try{
+        $json_res = $client->request('GET', "https://api.gnavi.co.jp/RestSearchAPI/v3/", ['query' => $params])->getBody();
+    }catch(Exception $e){
+        return print("エラーが発生しました。");
     }
-  }
-  return print_r($restaurants);
+    $response = json_decode($json_res,true);
 
+    if(isset($response["error"])){
+        return(print("エラーが発生しました！"));
+    }
+
+    foreach($response["rest"] as &$restaurant){
+        $rest_info = [$restaurant["name"],$restaurant["address"],$restaurant["opentime"],$restaurant["tel"]];
+        $restaurants[] = $rest_info;
+    }
+    $handle = fopen("restaurants_list.csv", "wb");
+
+    foreach ($restaurants as $values){
+        fputcsv($handle, $values);
+    }
+
+    fclose($handle);
+    return print_r($restaurants);
 }
-write_data_to_csv()
+write_data_to_csv($PARAMS);
+
 ?>
