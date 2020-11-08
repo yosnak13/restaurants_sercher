e3dfc932d95b49b5992106d37685e57911bb0966
yosnak13 <yosnak20@gmail.com>
2020-11-08 16:41:19 +0900

    restaurants_sercher完成

--- restaurants_searcher.php
+++ restaurants_searcher.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
 # 初期設定
-$KEYID = "1f19fb2ec4009251fef8fa7b4353473f";
+$KEYID =  getenv('GRNB_ACCESS_KEY');
 $HIT_PER_PAGE = 100;
 $PREF = "PREF13";
 $FREEWORD_CONDITION = 1;
@@ -13,32 +13,31 @@ $PARAMS = array("keyid"=> $KEYID, "hit_per_page"=>$HIT_PER_PAGE, "pref"=>$PREF, 
 
 
 function write_data_to_csv($params){
+  $restaurants = [["名称","住所","営業日","電話番号"]];
+  $client = new Client();
+  try{
+    $json_res = $client->request('GET', "https://api.gnavi.co.jp/RestSearchAPI/v3/", ['query' => $params])->getBody();
+  }catch(Exception $e){
+    return print("エラーが発生しました。");
+  }
+  $response = json_decode($json_res,true);
 
-    $restaurants = [["名称","住所","営業日","電話番号"]];
-    $client = new Client();
-    try{
-        $json_res = $client->request('GET', "https://api.gnavi.co.jp/RestSearchAPI/v3/", ['query' => $params])->getBody();
-    }catch(Exception $e){
-        return print("エラーが発生しました。");
-    }
-    $response = json_decode($json_res,true);
+  if(isset($response["error"])){
+    return(print("エラーが発生しました！"));
+  }
 
-    if(isset($response["error"])){
-        return(print("エラーが発生しました！"));
-    }
+  foreach($response["rest"] as &$restaurant){
+    $rest_info = [$restaurant["name"],$restaurant["address"],$restaurant["opentime"],$restaurant["tel"]];
+    $restaurants[] = $rest_info;
+  }
+  $handle = fopen("restaurants_list.csv", "wb");
 
-    foreach($response["rest"] as &$restaurant){
-        $rest_info = [$restaurant["name"],$restaurant["address"],$restaurant["opentime"],$restaurant["tel"]];
-        $restaurants[] = $rest_info;
-    }
-    $handle = fopen("restaurants_list.csv", "wb");
+  foreach ($restaurants as $values){
+    fputcsv($handle, $values);
+  }
 
-    foreach ($restaurants as $values){
-        fputcsv($handle, $values);
-    }
-
-    fclose($handle);
-    return print_r($restaurants);
+  fclose($handle);
+  return print_r($restaurants);
 }
 write_data_to_csv($PARAMS);
 
